ff9540642fbaeb7110eaf56feec5b5ffc578351f
rafalwaz <rafiwaz11@gmail.com>
2026-04-20 23:14:11 +0200

    Pierwszy commit

--- app.py
+++ app.py
@@ -7,10 +7,10 @@ load_dotenv() # Odpala ladowanie zmiennych z ukrytego .env!
 
 app = Flask(__name__)
 
-DB_HOST = os.getenv("DB_HOST", "localhost")
-DB_NAME = os.getenv("DB_NAME", "kalkulator")
-DB_USER = os.getenv("DB_USER", "postgres")
-DB_PASS = os.getenv("DB_PASS", "password")
+DB_HOST = os.getenv("DB_HOST")
+DB_NAME = os.getenv("DB_NAME")
+DB_USER = os.getenv("DB_USER")
+DB_PASS = os.getenv("DB_PASS")
 
 def get_db_connection():
     return psycopg2.connect(host=DB_HOST, database=DB_NAME, user=DB_USER, password=DB_PASS)
